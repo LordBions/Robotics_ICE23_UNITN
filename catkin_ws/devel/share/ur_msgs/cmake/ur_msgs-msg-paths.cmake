a1f5df9f805445756dc394d5930064462f3b64e8
LordBions <filippocb@outlook.it>
2023-04-13 22:59:44 +0200

    sync

---
+++ catkin_ws/devel/share/ur_msgs/cmake/ur_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(ur_msgs_MSG_INCLUDE_DIRS "/home/utente/Robotics_ICE23_UNITN/catkin_ws/src/robot/universal_robot/ur_msgs/msg")
+set(ur_msgs_MSG_DEPENDENCIES std_msgs)
