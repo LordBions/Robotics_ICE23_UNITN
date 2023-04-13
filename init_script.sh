73698522d82ef8bb6998534a715adfe87434e6fb
LordBions <filippocb@outlook.it>
2023-04-13 22:33:27 +0200

    mods

--- init_script.sh
+++ init_script.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-# setting up the catkin_ws directory
+
 # the main directory must be saved in $HOME
+cd $HOME
 
+# clone our repo
+git clone https://github.com/LordBions/Robotics_ICE23_UNITN
 
-cd $HOME/Robotics_ICE23_UNITN/catkin_ws
-catkin_make
+# installare tutte le dipendency ed i requirements nostri
+#............
+#............
+#............
+
+# set the source
 . $HOME/Robotics_ICE2023/catkin_ws/devel/setup.bash
 
+# write the source into bash rc
+echo "source $PWD/devel/setup.bash" >> $HOME/.bashrc
+
+# setting up the catkin_ws directory
+cd $HOME/Robotics_ICE23_UNITN/catkin_ws
+
+# build the catkin packages
+catkin_make
+
+# run environment
+roslaunch environment environment.launch
